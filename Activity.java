729f21594ddb802e0319b478850a9c05135d35fd
TrishiaPiliin <trishiamaepiliin28@gmail.com>
2024-09-12 15:17:40 +0800

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,12 @@
+class Activity {
+    public static void main(String args[]){
+
+        Dog dog = new Dog();
+        dog.name = "Bella";
+
+
+        System.out.println(dog.name);
+    }
+
+    
+}
---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog {
+
+    String name;
+    boolean isMale;
+    int numPuppies;
+    String myPuppies[] = new String[5];
+
+
+    public String showName() {
+        return "Name of the dog " + name;
+    }
+
+
+
+    
+}
