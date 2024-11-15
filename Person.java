2ba3a105bfb11741ca3bc1e0aee429a8ac9e496b
TrishiaPiliin <trishiamaepiliin28@gmail.com>
2024-11-15 14:13:16 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,22 @@
+class Person{
+    protected String Name;
+    protected String Address;
+
+public void setName(String Name){
+    this.Name = Name;
+}
+public void setAddress(String Address){
+    this.Address = Address;
+}
+
+public String getName(){
+    return Name;
+}
+public String getAddress(){
+    return Address;
+}
+
+public String toString(){
+    return "I am " + Name + "and" + "I lived in" + Address + ".";
+}
+}
